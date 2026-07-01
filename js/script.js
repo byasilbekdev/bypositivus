13baca69a4691178732d3842f0210d4224f8e65b
Asilbek <asilbekk.me@gmail.com>
2026-07-01 23:10:19 +0500

    git add

--- js/script.js
+++ js/script.js
@@ -2,29 +2,39 @@ const menuBtn = document.getElementById("menu-btn");
 const mobileMenu = document.getElementById("mobile-menu");
 const icon = menuBtn.querySelector("i");
 
-menuBtn.addEventListener("click", () => {
-  const isHidden = mobileMenu.classList.contains("hidden");
+let menuTimeout;
 
-  if (isHidden) {
+function toggleMenu(show) {
+  clearTimeout(menuTimeout);
+
+  if (show) {
     mobileMenu.classList.remove("hidden");
-    setTimeout(() => {
+
+    menuTimeout = setTimeout(() => {
       mobileMenu.classList.remove("opacity-0", "translate-y-[-10px]");
       mobileMenu.classList.add("opacity-100", "translate-y-0");
     }, 10);
+
     icon.classList.replace("fa-bars", "fa-xmark");
   } else {
     mobileMenu.classList.remove("opacity-100", "translate-y-0");
     mobileMenu.classList.add("opacity-0", "translate-y-[-10px]");
-    setTimeout(() => {
+
+    menuTimeout = setTimeout(() => {
       mobileMenu.classList.add("hidden");
     }, 300);
+
     icon.classList.replace("fa-xmark", "fa-bars");
   }
+}
+
+menuBtn.addEventListener("click", () => {
+  const isHidden = mobileMenu.classList.contains("hidden");
+  toggleMenu(isHidden);
 });
 
 mobileMenu.querySelectorAll("a").forEach((link) => {
   link.addEventListener("click", () => {
-    mobileMenu.classList.add("opacity-0", "translate-y-[-10px]", "hidden");
-    icon.classList.replace("fa-xmark", "fa-bars");
+    toggleMenu(false);
   });
 });
